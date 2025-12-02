1690804b731977f333bd6c6733c1b2b125effd52
gabrielcruzes <gabrielcruzes@gmail.com>
2025-12-02 18:57:37 -0300

    Update AsaasException.php

--- src/Payments/AsaasException.php
+++ src/Payments/AsaasException.php
@@ -7,6 +7,8 @@
 
 namespace App\Payments;
 
+require_once __DIR__ . '/PaymentException.php';
+
 class AsaasException extends PaymentException
 {
     public function __construct(string $message, int $statusCode = 502, ?\Throwable $previous = null)
